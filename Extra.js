495596f94ef3078a33ff92aff3b7c01c2db2269f
Mirali Moradiya <pramodpawar.work@gmail.com>
2024-11-07 12:00:46 +0530

    initial code added

---
+++ Extra.js
@@ -0,0 +1,20 @@
+1. git ne connect krnva mate : git init 
+atlu thse etle tmare file ni name ni baju ma U lakahi ne aavse etle hju file untracked che aapde ene track krva ni che 
+
+2. Get-ChildItem -Force - aana thi git nu ek hidden folder bnelu hse e btase 
+aama no jovu hoi to my pc ma view ma jvanu upar and ema checkbox hse last ma ema hidden name nu checkbox  check krvanu 
+
+3. have mare index.js file ne git ma tracked krvi che to : git add index.js 
+have file ni name ni baju ma u nhi A aavi jse 
+
+4. git diff - kai kai vastu add thai che ee btave
+
+5. aapdi pase multiple folders che and ema multiple files che to ene git ma eksathe add krva : 
+git add . 
+
+aana thi bdhi filse folder tracked thva mandse 
+
+
+6. particular file ne git mathi remove krva mate - git rm extra.js 
+
+ 
